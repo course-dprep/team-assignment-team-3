dee0dc78ae8ebaa0b75d9eba2f728622fbd116e8
BenteSchasfoort <111928362+BenteSchasfoort@users.noreply.github.com>
2022-10-05 18:43:49 +0200

    Update clean_data.R

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -1,11 +1,17 @@
+# Cleaning the Data #
+
+# Loading packages
+library(dplyr)
+
 # Load merged data 
-load("./gen/data-preparation/temp/data_merged.RData")
+load("./gen/temp/combined_data")
 
-# Drop observations with V1 <= -0.9
-df_cleaned <- df_merged[df_merged$V1 > -0.9,]
+## Create a dummy variable to identify the states where abortion is legal or not after overturning Roe v wade.  example lets assumed that countries with code 5,6, and 7 were treated (=1). states has the category "Legal" and "Gestational Limit" (abortion is banned after >= 15 weeks of pregnancy) are treated as "legal" (=0); states has the category "Banned" are treated as "banned"(=1) in our analysis. 
+combined_data$category1 <- ifelse(combined_data$category == "Legal", 0, 1)
+head(combined_data)
 
-# Remove V1
-df_cleaned <- df_cleaned[,c(1,2,4:7)]
+# Transform price to numeric
 
-# Save cleaned data
-save(df_cleaned,file="./gen/data-preparation/output/data_cleaned.RData")
+# Filtering the combined data on states, time, category & price
+combined_data1 <- subset(combined_data, select = c(price, states, time, category1))
+head(combined_data1)
