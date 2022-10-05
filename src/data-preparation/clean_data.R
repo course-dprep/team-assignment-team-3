a8d55b5cf80467e34fe68fc5badb1243bf21ffae
BenteSchasfoort <111928362+BenteSchasfoort@users.noreply.github.com>
2022-10-06 00:16:08 +0200

    Update clean_data.R

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -10,7 +10,7 @@ load("./gen/temp/combined_data")
 combined_data1 <- subset(combined_data, select = c("host_id", "price", "city", "states", "time", "category"))
 
 ## Create a dummy variable to identify the states where abortion is legal or not after overturning Roe v wade.  example lets assumed that countries with code 5,6, and 7 were treated (=1). states has the category "Legal" and "Gestational Limit" (abortion is banned after >= 15 weeks of pregnancy) are treated as "legal" (=0); states has the category "Banned" are treated as "banned"(=1) in our analysis. 
-combined_data$category1 <- ifelse(combined_data$category == "Legal", 0, 1)
+combined_data1$category1 <- ifelse(combined_data1$category == "Legal", 0, 1)
 head(combined_data)
 
 ## aggregate data to state level 
