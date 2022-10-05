d9fecd611b69821aea41186ead451918bc6c8322
BenteSchasfoort <111928362+BenteSchasfoort@users.noreply.github.com>
2022-10-05 18:18:23 +0200

    Update download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,8 +1,31 @@
-# Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset1.csv','./data/dataset1/dataset1.csv')
+# Downloading the Data #
 
-# Download dataset 2
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file('https://rgreminger.github.io/files/dataset2.csv','./data/dataset2/dataset2.csv')
+# Loading packages
+library(dplyr)
+library(googlesheets4)
+
+# Importing the Google Sheet in R.
+gs4_deauth()
+df <- read_sheet("https://docs.google.com/spreadsheets/d/1tOMjx-lflqp-9XC1iGc9tdBIfK-nrOdlsLnFGgLFdVA/edit#gid=0")
+url <- as.character(df$url)
+
+# Reading the datasets for all cities
+tbl <- lapply(url, function(url) {
+  print(paste0('Now downloading ... ', url))
+  d <- read.csv(url)
+  city = tolower(as.character(df$city[match(url, df$url)]))
+  d$city <- city
+  states = as.character(df$states[match(url, df$url)])
+  d$states <- states
+  time = df$time[match(url, df$url)]
+  d$time <- time
+  category = df$category[match(url, df$url)]
+  d$category <- category
+  return(d)
+})
+
+# Combining the data into a single data frame
+combined_data = do.call('rbind', tbl)
+head(combined_data)
+glimpse(combined_data)
 
